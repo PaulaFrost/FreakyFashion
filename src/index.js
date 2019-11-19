023b2a3b216fe3cf47990d27b633a5d80f245292
Paula Hernandez <43119682+PaulaFrost@users.noreply.github.com>
2019-11-19 13:43:22 +0100

    Ugly navbar in header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Navbar, Nav, Container, Row, Col, Form, FormControl, Button } from "react-bootstrap";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let menuItemList = [
+  { name: "Nyheter", url: "#" },
+  { name: "Kategorier", url: "#" },
+  { name: "Topplistan", url: "#" },
+  { name: "Rea", url: "#" },
+  { name: "Kampanjer", url: "#" },
+  { name: "Varumärke", url: "#" },
+  { name: "Shop", url: "#" },
+  { name: "Instagram", url: "#" }
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <header>
+      <Container fluid>
+          <Row>
+             <Col xs={2.5}><img src="https://via.placeholder.com/300x80?text=Logotype" alt="Logotype"></img> </Col>
+             <Col className="align-self-center ml-1">
+                <Form>
+                    <FormControl type="text" placeholder="Search" className="mr-sm2" size="sm"/>  
+                </Form>
+             </Col>
+             <Col className="align-self-center">
+                <Button variant="light" size="sm">Image search</Button>
+             </Col>
+          </Row>
+          <Row>
+            <Navbar expand="lg">
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        {menuItemList.map(menuItem => (
+                        <Nav.Link href={menuItem.url}>{menuItem.name}</Nav.Link>  
+                        ))}    
+                    </Nav>
+                </Navbar.Collapse> 
+            </Navbar>
+          </Row>
+      </Container>
+  </header>
+  ,
+  document.querySelector("#root")
+)
