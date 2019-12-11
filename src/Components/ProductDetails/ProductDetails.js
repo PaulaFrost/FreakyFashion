89e4202dbe9c865d7f7c1b3ad7b25d9647929895
Paula Hernandez <43119682+PaulaFrost@users.noreply.github.com>
2019-12-11 06:36:36 +0100

    fix productDetaisl

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,86 +1,85 @@
 import React, { useState } from "react";
 import "font-awesome/css/font-awesome.min.css";
-import {
-    Container,
-    Col,
-    Row,
-    Image,
-    Button,
-    Form
-  } from "react-bootstrap";
+import { Container, Col, Row, Image, Button, Form } from "react-bootstrap";
 
-const ProductDetails = (props) => {
-    
-    let [productName, setProductName] = useState(props.product.name);
-    let [productArtNr, setProductArtNr] = useState(props.product.artNr);
-    let [productPrice, setProductPrice] = useState(props.product.price);
-    let [productColor, setProductColor] = useState("");
-    let [productSize, setProductSize] = useState("M");
+const ProductDetails = product => {
+  let [productName] = useState(product.product.name);
+  let [productArtNr] = useState(product.product.artNr);
+  let [productPrice] = useState(product.product.price);
+  let [productColor, setProductColor] = useState("");
+  let [productSize, setProductSize] = useState("M");
 
-    let addToCart = () => {
+  let addToCart = () => {
+    const addedProduct = {
+      productName,
+      productArtNr,
+      productPrice,
+      productColor,
+      productSize
+    };
 
-        const addedProduct = {
-            productName,
-            productArtNr,
-            productPrice,
-            productColor,
-            productSize
-        };
+    console.log(addedProduct);
+  };
 
-        console.log(addedProduct);
-    }
-
-    return(
-        <Container className="mt-3">
-            <Row key={props.product.id}>
-                <Col md={5}>
-                    <Image fluid key={props.product.id} src={props.product.image} />
-                </Col>
-                <Col md={6}>
-                    <h2>{props.product.name}</h2>
-                    <p>{props.product.artNr}</p>
-                    <p>{props.product.description}</p>
-                    <h5><span>{props.product.price}</span> SEK</h5>
-                    <div>
-                        <label>
-                            Välj färg
-                            <Form>
-                            <fieldset>
-                            <Form.Group>
-                                {props.product.colors.map(color => (
-                                    <Form.Check 
-                                    key={color.id} 
-                                    inline label={color.name} 
-                                    type="radio" 
-                                    name="formHorizontalRadios"
-                                    value={color.name}
-                                    onChange={event => setProductColor(event.target.value)}
-                                    />
-                                ))}
-                            </Form.Group> 
-                            </fieldset>
-                            </Form>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Välj storlek
-                            <select
-                                className="ml-3"
-                                value={productSize}
-                                onChange={event => setProductSize(event.target.value)}>
-                                    {props.product.sizes.map(size => (
-                                        <option key={size.id} value={size.name}>{size.name}</option>
-                                    ))}
-                            </select>
-                        </label>
-                    </div>
-                    <Button onClick={addToCart} className="mt-3 mr-3">Köp</Button>
-                    <Button className="mt-3 mr-3">Spara</Button>
-                </Col>
-             </Row>
-        </Container>
-    )
-}
+  return (
+    <Container className="mt-3">
+      <Row key={product.product.id}>
+        <Col md={5}>
+          <Image fluid key={product.product.id} src={product.product.image} />
+        </Col>
+        <Col md={6}>
+          <h2>{product.product.name}</h2>
+          <p>{product.product.artNr}</p>
+          <p>{product.product.description}</p>
+          <h5>
+            <span>{product.product.price}</span> SEK
+          </h5>
+          <div>
+            <label>
+              Välj färg
+              <Form>
+                <fieldset>
+                  <Form.Group>
+                    {product.product.colors.map(color => (
+                      <Form.Check
+                        key={color.id}
+                        inline
+                        label={color.name}
+                        type="radio"
+                        name="formHorizontalRadios"
+                        value={color.name}
+                        onChange={event => setProductColor(event.target.value)}
+                      />
+                    ))}
+                  </Form.Group>
+                </fieldset>
+              </Form>
+            </label>
+          </div>
+          <div>
+            <label>
+              Välj storlek
+              <select
+                className="ml-3"
+                value={productSize}
+                onChange={event => setProductSize(event.target.value)}
+              >
+                {product.product.sizes.map(size => (
+                  <option key={size.id} value={size.name}>
+                    {size.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <Button onClick={addToCart} className="mt-3 mr-3">
+            Köp
+          </Button>
+          <Button className="mt-3 mr-3">Spara</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default ProductDetails;
